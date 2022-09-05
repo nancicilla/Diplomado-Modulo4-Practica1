9d733c4f3098f21b6ffcc4cfb4e0f3ea0eeb9632
Vallejos Guerrero Nancy <vallejosnancy2@gmail.com>
2022-09-04 23:05:24 -0400

    Methods Put an Delete

---
+++ controllers/productController.js
@@ -0,0 +1,100 @@
+const fs = require("fs");
+
+exports.getAllProducts = (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
+
+  res.status(200).json({
+    status: "success",
+    timeOfRequest: req.requestTime,
+    results: products.length,
+    data: {
+      products,
+    },
+  });
+};
+
+exports.addProduct = (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
+  products.push(req.body);
+  fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      products,
+    },
+  });
+};
+
+exports.getProductById = (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
+
+  const foundProduct = products.find((p) => p.id == req.params.id);
+  if (foundProduct) {
+    res.status(200).json({
+      status: "success",
+      data: {
+        product: foundProduct,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "not found",
+    });
+  }
+};
+
+exports.updateProduct = (req, res) => {
+  const products = JSON.parse(
+      fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
+  const foundProduct = products.find((p) => p.id == req.body.id);
+  if (foundProduct) {
+      products.splice(products.indexOf(foundProduct),1,req.body);
+      fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+      const listProducts = JSON.parse(
+          fs.readFileSync(`${__dirname}/../data/products.json`)
+      );
+      res.status(200).json({
+          status: "success",
+          data: {
+              products: listProducts,
+          },
+      });
+  } else {
+      res.status(404).json({
+          status: "not found",
+      });
+  }
+};
+exports.deleteProductById = (req, res) => {
+  const products = JSON.parse(
+      fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
+  const foundProduct = products.find((p) => p.id == req.params.id);
+  if (foundProduct) {
+      //fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify([]));
+      products.splice(products.indexOf(foundProduct), 1);
+      fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+      const listProducts = JSON.parse(
+          fs.readFileSync(`${__dirname}/../data/products.json`)
+      );
+      
+      res.status(200).json({
+          status: "success",
+          data: {
+              products: listProducts,
+          },
+      });
+  } else {
+      res.status(404).json({
+          status: "not found",
+      });
+  }
+};
