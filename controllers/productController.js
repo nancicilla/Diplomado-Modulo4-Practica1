f51a973bb0bfdf43fd7e009c94767a44b9e2fa96
Nancy <41241255+nancicilla@users.noreply.github.com>
2022-09-06 23:45:45 -0400

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,88 +1,81 @@
 
-const Product=require("../models/Products");
-exports.getAllProducts = async (req, res) => {
-  
-  const products=await Product.find();
-  res.status(200).json({
-    status: "success",
-    timeOfRequest: req.requestTime,
-    results: products.length,
-    data: {
-      products,
-    },
-  });
-};
+const Product = require("../models/Products");
+exports.getAllProducts = async(req, res) => {
 
-exports.addProduct = async (req, res) => {
- const newProduct=await Product.create(req.body);
-  res.status(200).json({
-    status: "success",
-    data: {
-      newProduct,
-    },
-  });
-};
-
-exports.getProductById =async (req, res) => {
-  const foundProduct =await Product.findById(req.params.id);
-  if (foundProduct) {
+    const products = await Product.find();
     res.status(200).json({
-      status: "success",
-      data: {
-        product: foundProduct,
-      },
+        status: "success",
+        timeOfRequest: req.requestTime,
+        results: products.length,
+        data: {
+            products,
+        },
     });
-  } else {
-    res.status(404).json({
-      status: "not found",
-    });
-  }
 };
 
-exports.updateProduct =  (req, res) => {
-  const productUpdate=  Product.findByIdAndUpdate(req.body._id,req.body,{new: true},
-  function (err, response) {
-      // Handle any possible database errors
-      if (err) {
-        console.log(" error=" + err);
-        res.json({
-          message: 'Database Update Failure'
-        });
-      }
-      
+exports.addProduct = async(req, res) => {
+    const newProduct = await Product.create(req.body);
+    res.status(200).json({
+        status: "success",
+        data: {
+            newProduct,
+        },
     });
- 
-  if (productUpdate) {
-     
-      res.status(200).json({
-          status: "success",
-          message:"product updated"
-      });
-  } else {
-      res.status(404).json({
-          status: "not found",
-      });
-  }
+};
+
+exports.getProductById = async(req, res) => {
+    const foundProduct = await Product.findById(req.params.id);
+    if (foundProduct) {
+        res.status(200).json({
+            status: "success",
+            data: {
+                product: foundProduct,
+            },
+        });
+    } else {
+        res.status(404).json({
+            status: "not found",
+        });
+    }
+};
+
+exports.updateProduct = (req, res) => {
+    const productUpdate = Product.findByIdAndUpdate(req.body._id, req.body, { new: true },
+        function(err, response) {
+            // Handle any possible database errors
+            if (err) {
+                res.status(404).json({
+                    status: "not found",
+                });
+            } else {
+                res.status(200).json({
+                    status: "success",
+                    message: "product updated"
+                });
+            }
+
+        });
+
+
 };
 exports.deleteProductById = (req, res) => {
-  
-  const productDeleted =Product.findByIdAndDelete(req.params.id,function (err, document) {
-    if (err){
-        console.log(err)
-    }
-    else{
-        console.log("Product deleted : ", document);
-    }});
-  
-  if (productDeleted) {
-     
-      res.status(200).json({
-          status: "success",
-          message:"Product deleted"
-      });
-  } else {
-      res.status(404).json({
-          status: "not found",
-      });
-  }
-};
+
+    Product.findByIdAndDelete(req.params.id, function(err, document) {
+        if (err) {
+            console.log(err)
+            res.status(404).json({
+                status: "not found",
+            });
+        } else {
+            console.log("Product deleted : ", document);
+            res.status(200).json({
+                status: "success",
+                message: "Product deleted"
+            });
+        }
+    });
+
+
+
+
+}
